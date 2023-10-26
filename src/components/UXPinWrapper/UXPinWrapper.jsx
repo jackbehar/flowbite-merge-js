323f59e13d7cb2aad8f11620ea5504a5d01da7da
Jack Behar <84526167+jackbehar@users.noreply.github.com>
2023-10-26 15:20:32 -0600

    Added theme

--- src/components/UXPinWrapper/UXPinWrapper.jsx
+++ src/components/UXPinWrapper/UXPinWrapper.jsx
@@ -1,7 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { Flowbite } from 'flowbite-react';
+
 import './index.css'
 // eslint-disable-next-line react/prop-types
+
+
+const customTheme= {
+  button: {
+    color: {
+      primary: 'bg-red-500 hover:bg-red-600',
+    },
+  },
+};
+
+
+
 export default function UXPinWrapper({ children }) {
-  return <div>{children}</div>;
+  
+  return <Flowbite theme={{ theme: customTheme }}>
+    {children}
+  </Flowbite>;
 }
